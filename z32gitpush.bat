da5412c4d50f0cec6e0ac4492673a0cd3964960b
RayBowman2017b <RayBowman2017b@gmail.com>
2018-10-06 14:00:20 -0500

    2018-10-06 14-02 2nd commit

--- z32gitpush.bat
+++ z32gitpush.bat
@@ -2,5 +2,5 @@
 @rem 
 @rem git remote add Budget_app_03 git@github.com:RayBowman2017b/react-course-2-budget-app_03.git
 @rem git remote add Budget_app_03 git@github.com:RayBowman2017b/react-course-2-budget-app_03b.git
-git push -u Budget_app_03b master
+git push -u Budget-app_L136b master
 pause
